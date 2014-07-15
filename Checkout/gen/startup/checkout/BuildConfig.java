ec9a76ee6e63a09361efc5ecfe202fe31fd3a862
Sunny Deo <Deo.Abhishek4@gmail.com>
2014-07-15 00:40:51 -0400

    New File

---
+++ Checkout/gen/startup/checkout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package startup.checkout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-CheckoutOptimizer
-=================
-Application created by Sagar Laud, Karan Pahawa, and Abhishek Deo in an attempt to optimize checkout at all stores.
